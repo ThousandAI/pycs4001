abb173ef046e7f51b2e542982e2d343ba3a15b6a
thousandai <thousand.ai.tech@gmail.com>
2022-07-04 01:59:01 +0800

    class04 editing

--- class04/main.py
+++ class04/main.py
@@ -1,3 +1,20 @@
+# 函式
+"""
+def get_sum(course,*args):
+    scores = sum(args)
+    print(f"{course} 總分為: {scores}")
+
+get_sum("Math",30,20,60,80,100,90,70,98)
+
+def get_sports(sports, **kwargs):
+    print(sports)
+    print(kwargs)
+    for k,v in kwargs.items():
+        print(f"{k}: {v}")
+
+get_sports( "basketball",curry=[15,10,5,10], thompson= [20,10,10,5] )
+"""
+
 # dictionary
 """
 dic = {"sports":["basketball", "football"], "name": ["Allie", "Thousand"]}
@@ -112,3 +129,119 @@ class Magician:
 """
 
 # inheritance
+"""
+class Character:
+    def __init__(self, name, gender, weapon="bat"):
+        self.lv = 1
+        self.exp = 0
+        self.n = name
+        self.g = gender
+        self.w = weapon
+    print("This is Character class.")
+    def __str__(self):
+        return f"Name: {self.n}, Gender: {self.g}, Weapon: {self.w}, Level: {self.lv}, Exp: {self.exp}"
+
+    def attack(self, enemy):
+        self.exp += 40
+        enemy.hp -= 1
+        self.level_up()
+        print(f"{self.n} attacked! Level: {self.lv}, Exp: {self.exp}, Enemy Hp: {enemy.hp}", end = " ")
+
+    def level_up(self):
+        if self.exp >= 100:
+            self.lv += 1
+            self.exp -= 100
+
+class Warrior(Character):
+    def __init__(self, name, gender, weapon="bat"):
+        super().__init__(name, gender, weapon)
+        self.sp = 100
+
+    # override
+    def attack(self,enemy):
+        super().attack(enemy)
+        #print(f"{self.n} attacked {enemy.n} with {self.w}")
+        print(f"with {self.w}")
+
+    def bash(self, enemy):
+        if self.lv >= 5:
+            print(f"{self.n} used bash.")
+            enemy.hp -= 10
+        else:
+            print("等級5才能使用")
+
+class Magician(Character):
+    def __init__(self, name, gender, weapon="bat"):
+        super().__init__(name, gender, weapon)
+        self.mp = 100
+
+    def fireball(self,enemy):
+        if self.lv >= 5:
+            print(f"{self.n} used fireball.")
+            enemy.hp -= 10
+        else:
+            print("等級5才能使用")
+
+class Enemy:
+    def __init__(self):
+        self.n = 'Monster'
+        self.hp = 100
+
+warrior1 = Warrior(name="Thousand", gender="Male", weapon="Sword")
+magician1 = Magician(name="Allie", gender="Female", weapon="Wand")
+e1 = Enemy()
+
+print(warrior1)
+print(magician1)
+
+for i in range(10):
+    warrior1.attack(enemy=e1) # override
+
+warrior1.bash(enemy=e1)
+magician1.fireball(enemy=e1)
+"""
+
+import numpy as np
+
+# numpy basic
+"""
+a = np.array([1,2,3])
+b = np.array([[1.0,2.0,3.0],[3.2,5.7,8.2]])
+print(f"a: {a}")
+print(f"a dimension: {a.ndim}")
+print(f"a shape: {a.shape}")
+print(f"a type: {a.dtype}")
+print(f"b: {b}")
+print(f"b dimension: {b.ndim}")
+print(f"b shape: {b.shape}")
+print(f"b type: {b.dtype}")
+"""
+
+# index/slice/2D
+"""
+a = np.array([[1,2,3,4,5,6],[7,8,9,10,11,12]])
+print(a[1,2]) # 8
+print(a[0,:]) # [1,2,3,4,5]
+print(a[:,2]) # [3,8]
+print(a[0,1:5:2]) # [2,4] (start,stop,step)
+a[1,2] = 20
+print(a)
+a[:,2] = [7,8]
+print(a)
+"""
+
+# array 必須要儲存同一種型態的資料
+"""
+a = [1,2,3]
+b = [4,5,6]
+c = []
+for i in range(len(a)):
+    c.append(a[i] + b[i])
+print(c)
+print(a+b)  # 相加是串列相加
+
+x = np.array([1,2,3])
+y = np.array([4,5,6])
+print(x+y) # 平行運算
+
+"""
