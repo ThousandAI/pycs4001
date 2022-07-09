ec28ce4f4f3cae291478ff346c42feff9611d408
thousandai <thousand.ai.tech@gmail.com>
2022-07-09 21:06:18 +0800

    class05 editing

---
+++ class05/main.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# DataFrame
+"""
+dic = {"Sports":["Basketball", "Soccer", "Tennis", "Volleyball", "Badminton"],
+       "Fruits":["Watermelon", "Banana", "Apple", "Lemon", "Strawberry"],
+       "Animals": ["Monkey", "Ape", "Dog", "Cat", "Lion"]}
+
+
+data = pd.DataFrame(dic)
+print(type(data))
+print(data)
+
+# series
+new_data = data["Sports"]
+print(type(new_data))
+print(new_data)
+
+# 新增
+data["Name"] = ["Thousand", "Allie", "John", "Michale", "Frank"]
+print(data)
+
+# 取值
+print(data.iloc[1:4, 1:3])
+"""
+
+# Read file
+
+
+
